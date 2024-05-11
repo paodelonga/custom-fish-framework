e5b0ddd1deadaacab62cdbe1c5fd5719df5e24f8
paodelonga <72471292+paodelonga@users.noreply.github.com>
2024-05-11 20:00:58 -0300

    creating new list iteration block

--- fish/custom/keybindings/init.fish
+++ fish/custom/keybindings/init.fish
@@ -9,11 +9,19 @@ function __init__
     set -a module_list "fzf_default_key_bindings.fish"
     set -a module_list "fzf_user_key_bindings.fish"
 
+    # iterate items in module_list
+    for _idx in $module_list
+        if test -d "$custom_path/$_idx"
+            echo "directory: $custom_path/$_idx"
+        else if test -f "$custom_path/$_idx"
+            echo "file: $custom_path/$_idx"
+        else
+            echo "WHAT HELL IS: $custom_path/$_idx"
+        end
 
-    for _file in (ls "$custom_path" --ignore 'init.fish')
-        source "$custom_path/$_file" ||
-            echo "cannot load $custom_path/$_file"
-        eval (basename -s '.fish' "$_file")
+        # source "$custom_path/$_file" ||
+        #     echo "cannot load $custom_path/$_file"
+        # eval (basename -s '.fish' "$_file")
     end
 end
 
