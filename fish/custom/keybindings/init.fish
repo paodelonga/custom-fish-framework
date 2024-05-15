3f6c80c87f8877739d2db0cb560061d34c6c699a
paodelonga <72471292+paodelonga@users.noreply.github.com>
2024-05-14 21:38:54 -0300

    add module loading in interation block

--- fish/custom/keybindings/init.fish
+++ fish/custom/keybindings/init.fish
@@ -5,23 +5,18 @@ function __init__
     set custom_path (dirname (status -f))
 
     # set modules names here
-    set -a module_list "fish_user_key_bindings.fish"
-    set -a module_list "fzf_default_key_bindings.fish"
-    set -a module_list "fzf_user_key_bindings.fish"
+    set -a module_list "fish_user_key_bindings"
+    set -a module_list "fzf_default_key_bindings"
+    set -a module_list "fzf_user_key_bindings"
 
     # iterate items in module_list
     for _idx in $module_list
-        if test -d "$custom_path/$_idx"
-            echo "directory: $custom_path/$_idx"
-        else if test -f "$custom_path/$_idx"
-            echo "file: $custom_path/$_idx"
+        if test -r "$custom_path/$_idx.fish"
+            source "$custom_path/$_idx.fish" &&
+            eval (basename -s '.fish' "$_idx")
         else
             echo "WHAT HELL IS: $custom_path/$_idx"
         end
-
-        # source "$custom_path/$_file" ||
-        #     echo "cannot load $custom_path/$_file"
-        # eval (basename -s '.fish' "$_file")
     end
 end
 
